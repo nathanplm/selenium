d0b3683fb7e4bb5763102e4473969d45f1d8c57a
Nathan <76407978+nathanplm@users.noreply.github.com>
2022-01-25 14:30:34 +0100

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,37 @@
+<?php
+require('header.php');
+require('nav.php');
+?>
+<link rel="stylesheet" href="assets/css/main.css">
+
+<div class="container container-fluid">
+    
+    <h2>Selenium, c'est quoi ?</h2>
+
+    <p>
+        Selenium est un framework de tests informatiques développé en Java mais qui propose certains outils pour pouvoir être utilisé avec différents langages comme Python ou PHP.
+        Il permet d'intéragir avec différents navigateurs Web (Google Chrome, Mozilla Firefox). <br>
+        C’est un outil de test dit dynamique (à l’inverse des tests statiques qui ne nécessitent pas l’exécution du logiciel) ce qui facilite le test fonctionnel.
+    </P>
+    <br>
+    <p>
+        La version actuelle de Selenium utilise principalement le HTML et le JavaScript. Cela permet aux développeurs de tester et d'enregistrer les interactions avec une application Web. <br>
+        Le principal intérêt est de pouvoir automatiser ces interactions afin de les répéter autant de fois que nécessaire. 
+        Les principales composantes de ce processus de test sont les suivantes :
+    </p>
+    <br><br>
+
+    <h2>Selenium IDE</h2>
+
+    <p>
+    Selenium IDE est l’environnement de développement du framework de test qui sert notamment de base à l’extension IDE pour Chrome et Firefox. Cet environnement est nécessaire pour l’enregistrement 
+    et la lecture des tests. <br>
+    Cette extension a été développée dans l'objectif d'utiliser Selenium sans passer par un serveur pour l'exécution des scripts.
+    </p>
+    <h2>Selenium WebDriver</h2>
+    <h2>Selenium Core</h2>
+    <h2>Selenium Grid</h2>
+
+
+</div>
+
---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Selenium</title>
+        <!--<link rel="icon" type="image/png" href="/assets/img/logo.png" />-->
+        <link rel="stylesheet" href="assets/css/main.css">
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <!-- Fontawesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
+    </head>
+    <body>
+        <div class="container-fluid banner" style="background-color:#fffae7; height:120px;">
+            <img src="assets/img/iut_bdx.png" class="img_iut">
+            <img src="assets/img/logo.png" class="img_logo">
+        </div>
